edfe03525dd9907bc783ff3c1515dc1629e008b2
Pablo Marques <pahbloo@gmail.com>
2021-02-21 12:38:56 -0300

    ✅ add tests

--- javascript-algorithms-and-data-structures-certification/roman-numeral-converter/script.js
+++ javascript-algorithms-and-data-structures-certification/roman-numeral-converter/script.js
@@ -17,67 +17,112 @@ function convertToRoman(num) {
 let code = document.getElementById("my-solution");
 code.innerHTML = convertToRoman.toString();
 
-//TODO: insert tests for convertToRoman()
 tests = [
   {
-    text: "<code>palindrome(&quot;eye&quot;)</code> should return a boolean.",
-    code: typeof palindrome("eye") === "boolean",
+    text: "<code>convertToRoman(2)</code> should return &quot;II&quot;.",
+    code: convertToRoman(2) === "II",
   },
   {
-    text: "<code>palindrome(&quot;eye&quot;)</code> should return true.",
-    code: palindrome("eye") === true,
+    text: "<code>convertToRoman(3)</code> should return &quot;III&quot;.",
+    code: convertToRoman(3) === "III",
   },
   {
-    text: "<code>palindrome(&quot;_eye&quot;)</code> should return true.",
-    code: palindrome("_eye") === true,
+    text: "<code>convertToRoman(4)</code> should return &quot;IV&quot;.",
+    code: convertToRoman(4) === "IV",
   },
   {
-    text: "<code>palindrome(&quot;race car&quot;)</code> should return true.",
-    code: palindrome("race car") === true,
+    text: "<code>convertToRoman(5)</code> should return &quot;V&quot;.",
+    code: convertToRoman(5) === "V",
+  },
+  {
+    text: "<code>convertToRoman(9)</code> should return &quot;IX&quot;.",
+    code: convertToRoman(9) === "IX",
+  },
+  {
+    text: "<code>convertToRoman(12)</code> should return &quot;XII&quot;.",
+    code: convertToRoman(12) === "XII",
+  },
+  {
+    text: "<code>convertToRoman(16)</code> should return &quot;XVI&quot;.",
+    code: convertToRoman(16) === "XVI",
+  },
+  {
+    text: "<code>convertToRoman(29)</code> should return &quot;XXIX&quot;.",
+    code: convertToRoman(29) === "XXIX",
+  },
+  {
+    text: "<code>convertToRoman(44)</code> should return &quot;XLIV&quot;.",
+    code: convertToRoman(44) === "XLIV",
+  },
+  {
+    text: "<code>convertToRoman(45)</code> should return &quot;XLV&quot;.",
+    code: convertToRoman(45) === "XLV",
+  },
+  {
+    text: "<code>convertToRoman(68)</code> should return &quot;LXVIII&quot;.",
+    code: convertToRoman(68) === "LXVIII",
+  },
+  {
+    text: "<code>convertToRoman(83)</code> should return &quot;LXXXIII&quot;.",
+    code: convertToRoman(83) === "LXXXIII",
+  },
+  {
+    text: "<code>convertToRoman(97)</code> should return &quot;XCVII&quot;.",
+    code: convertToRoman(97) === "XCVII",
+  },
+  {
+    text: "<code>convertToRoman(99)</code> should return &quot;XCIX&quot;.",
+    code: convertToRoman(99) === "XCIX",
+  },
+  {
+    text: "<code>convertToRoman(400)</code> should return &quot;CD&quot;.",
+    code: convertToRoman(400) === "CD",
+  },
+  {
+    text: "<code>convertToRoman(500)</code> should return &quot;D&quot;.",
+    code: convertToRoman(500) === "D",
+  },
+  {
+    text: "<code>convertToRoman(501)</code> should return &quot;DI&quot;.",
+    code: convertToRoman(501) === "DI",
+  },
+  {
+    text: "<code>convertToRoman(649)</code> should return &quot;DCXLIX&quot;.",
+    code: convertToRoman(649) === "DCXLIX",
   },
   {
     text:
-      "<code>palindrome(&quot;not a palindrome&quot;)</code> should return false.",
-    code: palindrome("not a palindrome") === false,
+      "<code>convertToRoman(798)</code> should return &quot;DCCXCVIII&quot;.",
+    code: convertToRoman(798) === "DCCXCVIII",
+  },
+  {
+    text: "<code>convertToRoman(891)</code> should return &quot;DCCCXCI&quot;.",
+    code: convertToRoman(891) === "DCCCXCI",
+  },
+  {
+    text: "<code>convertToRoman(1000)</code> should return &quot;M&quot;.",
+    code: convertToRoman(1000) === "M",
+  },
+  {
+    text: "<code>convertToRoman(1004)</code> should return &quot;MIV&quot;.",
+    code: convertToRoman(1004) === "MIV",
+  },
+  {
+    text: "<code>convertToRoman(1006)</code> should return &quot;MVI&quot;.",
+    code: convertToRoman(1006) === "MVI",
+  },
+  {
+    text: "<code>convertToRoman(1023)</code> should return &quot;MXXIII&quot;.",
+    code: convertToRoman(1023) === "MXXIII",
+  },
+  {
+    text: "<code>convertToRoman(2014)</code> should return &quot;MMXIV&quot;.",
+    code: convertToRoman(2014) === "MMXIV",
   },
   {
     text:
-      "<code>palindrome(&quot;A man, a plan, a canal. Panama&quot;)</code> should return true.",
-    code: palindrome("A man, a plan, a canal. Panama") === true,
-  },
-  {
-    text:
-      "<code>palindrome(&quot;never odd or even&quot;)</code> should return true.",
-    code: palindrome("never odd or even") === true,
-  },
-  {
-    text: "<code>palindrome(&quot;nope&quot;)</code> should return false.",
-    code: palindrome("nope") === false,
-  },
-  {
-    text:
-      "<code>palindrome(&quot;almostomla&quot;)</code> should return false.",
-    code: palindrome("almostomla") === false,
-  },
-  {
-    text:
-      "<code>palindrome(&quot;My age is 0, 0 si ega ym.&quot;)</code> should return true.",
-    code: palindrome("My age is 0, 0 si ega ym.") === true,
-  },
-  {
-    text:
-      "<code>palindrome(&quot;1 eye for of 1 eye.&quot;)</code> should return false.",
-    code: palindrome("1 eye for of 1 eye.") === false,
-  },
-  {
-    text:
-      "<code>palindrome(&quot;0_0 (: /- :) 0-0&quot;)</code> should return true.",
-    code: palindrome("0_0 (: /- :) 0-0") === true,
-  },
-  {
-    text:
-      "<code>palindrome(&quot;five|_/|four&quot;)</code> should return false.",
-    code: palindrome("five|_/|four") === false,
+      "<code>convertToRoman(3999)</code> should return &quot;MMMCMXCIX&quot;.",
+    code: convertToRoman(3999) === "MMMCMXCIX",
   },
 ];
 
