431228175c9f7d57f1a70acf32878cd293dfccbf
Pablo Marques <pahbloo@gmail.com>
2021-03-18 00:08:24 -0300

    ♻ refactoring js projects

--- javascript-algorithms-and-data-structures-certification/script.js
+++ javascript-algorithms-and-data-structures-certification/script.js
@@ -8,6 +8,127 @@ function palindrome(str) {
     .every((letter, index) => letter === arr.slice(-1 - index)[0]);
 }
 
+function convertToRoman(num) {
+  if (num >= 4000) return;
+  const roman = ` M MM MMM
+ C CC CCC CD D DC DCC DCCC CM
+ X XX XXX XL L LX LXX LXXX XC
+ I II III IV V VI VII VIII IX`
+    .split("\n")
+    .map((el) => el.split(" "));
+
+  return num
+    .toString()
+    .padStart(4, 0)
+    .split("")
+    .map((digit, index) => roman[index][digit])
+    .join("");
+}
+
+function rot13(str) {
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  const shift = 13;
+
+  return str
+    .split("")
+    .map((char) => {
+      const charIndex = alphabet.indexOf(char);
+      return charIndex === -1
+        ? char
+        : charIndex >= shift
+        ? alphabet[charIndex - shift]
+        : alphabet[alphabet.length - shift + charIndex];
+    })
+    .join("");
+}
+
+function telephoneCheck(str) {
+  const regex = new RegExp(
+    "^1? ?" + //optional USA country code
+      "(\\(\\d{3}\\)|\\d{3})" + // area code
+      "[ -]?\\d{3}" + //first group
+      "[ -]?\\d{4}$" //last group
+  );
+  return regex.test(str);
+}
+
+function checkCashRegister(price, cash, cid) {
+  //To avoid rounding problems
+  const times100 = (x) => Math.floor(100 * x);
+  cash = times100(cash);
+  price = times100(price);
+
+  const values = {
+    PENNY: 1,
+    NICKEL: 5,
+    DIME: 10,
+    QUARTER: 25,
+    ONE: 100,
+    FIVE: 500,
+    TEN: 1000,
+    TWENTY: 2000,
+    "ONE HUNDRED": 10000,
+  };
+
+  const registerCash = cid.reduce(
+    (total, [, value]) => times100(value) + total,
+    0
+  );
+
+  if (cash - price > registerCash)
+    return {
+      status: "INSUFFICIENT_FUNDS",
+      change: [],
+    };
+
+  if (cash - price === registerCash)
+    return {
+      status: "CLOSED",
+      change: cid,
+    };
+
+  let obj = cid.reduceRight(
+    (obj, el) => {
+      let unit = el[0];
+      let value = values[unit];
+      let amount = times100(el[1]);
+      let remainder =
+        cash -
+        price -
+        obj.change.reduce((given, [, value]) => given + value, 0);
+
+      //There's no way to return the exact change
+      if (unit === "PENNY" && remainder > amount)
+        return {
+          status: "INSUFFICIENT_FUNDS",
+          change: [],
+        };
+
+      //All the change was given
+      if (remainder <= 0) {
+        return obj;
+      }
+
+      //There's no way to give this bill as change
+      if (value > remainder) return obj;
+
+      //Give all of this bill
+      if (amount < remainder) {
+        obj.change.push([el[0], times100(el[1])]);
+        return obj;
+      }
+
+      obj.change.push([unit, Math.floor(remainder / value) * value]);
+      return obj;
+    },
+    { status: "OPEN", change: [] }
+  );
+
+  obj.change = obj.change.map((el) => [el[0], el[1] / 100]);
+
+  return obj;
+}
+
 const projects = {
   "palindrome-checker": {
     title: "Palindrome Checker",
@@ -101,19 +222,150 @@ const projects = {
     ],
   },
   "roman-numeral-converter": {
-    title: "",
+    title: "Roman Numeral Converter",
+    problem: `<p>Convert the given number into a roman numeral.</p>
+      <p>
+        All
+        <a href="http://www.mathsisfun.com/roman-numerals.html">roman numerals</a>
+        answers should be provided in upper-case.
+      </p>`,
+    solution: convertToRoman,
+    tests: [
+      {
+        text: "<code>convertToRoman(2)</code> should return &quot;II&quot;.",
+        code: convertToRoman(2) === "II",
+      },
+      {
+        text: "<code>convertToRoman(3)</code> should return &quot;III&quot;.",
+        code: convertToRoman(3) === "III",
+      },
+      {
+        text: "<code>convertToRoman(4)</code> should return &quot;IV&quot;.",
+        code: convertToRoman(4) === "IV",
+      },
+      {
+        text: "<code>convertToRoman(5)</code> should return &quot;V&quot;.",
+        code: convertToRoman(5) === "V",
+      },
+      {
+        text: "<code>convertToRoman(9)</code> should return &quot;IX&quot;.",
+        code: convertToRoman(9) === "IX",
+      },
+      {
+        text: "<code>convertToRoman(12)</code> should return &quot;XII&quot;.",
+        code: convertToRoman(12) === "XII",
+      },
+      {
+        text: "<code>convertToRoman(16)</code> should return &quot;XVI&quot;.",
+        code: convertToRoman(16) === "XVI",
+      },
+      {
+        text: "<code>convertToRoman(29)</code> should return &quot;XXIX&quot;.",
+        code: convertToRoman(29) === "XXIX",
+      },
+      {
+        text: "<code>convertToRoman(44)</code> should return &quot;XLIV&quot;.",
+        code: convertToRoman(44) === "XLIV",
+      },
+      {
+        text: "<code>convertToRoman(45)</code> should return &quot;XLV&quot;.",
+        code: convertToRoman(45) === "XLV",
+      },
+      {
+        text:
+          "<code>convertToRoman(68)</code> should return &quot;LXVIII&quot;.",
+        code: convertToRoman(68) === "LXVIII",
+      },
+      {
+        text:
+          "<code>convertToRoman(83)</code> should return &quot;LXXXIII&quot;.",
+        code: convertToRoman(83) === "LXXXIII",
+      },
+      {
+        text:
+          "<code>convertToRoman(97)</code> should return &quot;XCVII&quot;.",
+        code: convertToRoman(97) === "XCVII",
+      },
+      {
+        text: "<code>convertToRoman(99)</code> should return &quot;XCIX&quot;.",
+        code: convertToRoman(99) === "XCIX",
+      },
+      {
+        text: "<code>convertToRoman(400)</code> should return &quot;CD&quot;.",
+        code: convertToRoman(400) === "CD",
+      },
+      {
+        text: "<code>convertToRoman(500)</code> should return &quot;D&quot;.",
+        code: convertToRoman(500) === "D",
+      },
+      {
+        text: "<code>convertToRoman(501)</code> should return &quot;DI&quot;.",
+        code: convertToRoman(501) === "DI",
+      },
+      {
+        text:
+          "<code>convertToRoman(649)</code> should return &quot;DCXLIX&quot;.",
+        code: convertToRoman(649) === "DCXLIX",
+      },
+      {
+        text:
+          "<code>convertToRoman(798)</code> should return &quot;DCCXCVIII&quot;.",
+        code: convertToRoman(798) === "DCCXCVIII",
+      },
+      {
+        text:
+          "<code>convertToRoman(891)</code> should return &quot;DCCCXCI&quot;.",
+        code: convertToRoman(891) === "DCCCXCI",
+      },
+      {
+        text: "<code>convertToRoman(1000)</code> should return &quot;M&quot;.",
+        code: convertToRoman(1000) === "M",
+      },
+      {
+        text:
+          "<code>convertToRoman(1004)</code> should return &quot;MIV&quot;.",
+        code: convertToRoman(1004) === "MIV",
+      },
+      {
+        text:
+          "<code>convertToRoman(1006)</code> should return &quot;MVI&quot;.",
+        code: convertToRoman(1006) === "MVI",
+      },
+      {
+        text:
+          "<code>convertToRoman(1023)</code> should return &quot;MXXIII&quot;.",
+        code: convertToRoman(1023) === "MXXIII",
+      },
+      {
+        text:
+          "<code>convertToRoman(2014)</code> should return &quot;MMXIV&quot;.",
+        code: convertToRoman(2014) === "MMXIV",
+      },
+      {
+        text:
+          "<code>convertToRoman(3999)</code> should return &quot;MMMCMXCIX&quot;.",
+        code: convertToRoman(3999) === "MMMCMXCIX",
+      },
+    ],
+  },
+  "caesars-cipher": {
+    title: "Caesars Cipher",
     problem: "",
-    solution: "",
+    solution: rot13,
     tests: [],
   },
-  "caesar-cipher": { title: "", problem: "", solution: "", tests: [] },
   "telephone-number-validator": {
-    title: "",
+    title: "Telephone Number Validator",
     problem: "",
-    solution: "",
+    solution: telephoneCheck,
     tests: [],
   },
-  "cash-register": { title: "", problem: "", solution: "", tests: [] },
+  "cash-register": {
+    title: "Cash Register",
+    problem: "",
+    solution: checkCashRegister,
+    tests: [],
+  },
 };
 
 const id = (identifier) => document.getElementById(identifier);
@@ -123,7 +375,8 @@ let problem = id("problem");
 let solution = id("my-solution");
 let tests = id("tests");
 
-let project = "palindrome-checker";
+const URLParams = new URL(document.location).searchParams;
+let project = URLParams.get("p") || "palindrome-checker";
 
 title.innerHTML = projects[project].title;
 problem.innerHTML = projects[project].problem;
